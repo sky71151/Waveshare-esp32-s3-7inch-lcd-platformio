b14e1e387436c498cd8ae17cfb8108cef9479fab
sky71151 <110427423+sky71151@users.noreply.github.com>
2024-07-18 17:53:07 +0200

    first commit

---
+++ esp32/src/app.cpp
@@ -0,0 +1,61 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+#include <Arduino.h>
+#include "ESP_Panel_Library.h"
+#include "lvgl.h"
+#include "lvgl_port_v8.h"
+#include "ui.h"
+
+/**
+/* To use the built-in examples and demos of LVGL uncomment the includes below respectively.
+ * You also need to copy `lvgl/examples` to `lvgl/src/examples`. Similarly for the demos `lvgl/demos` to `lvgl/src/demos`.
+ */
+// #include <demos/lv_demos.h>
+// #include <examples/lv_examples.h>
+
+void setup()
+{
+    String title = "LVGL porting example";
+
+    Serial.begin(115200);
+    Serial.println(title + " start");
+
+    Serial.println("Initialize panel device");
+    ESP_Panel *panel = new ESP_Panel();
+    panel->init();
+#if LVGL_PORT_AVOID_TEAR
+    // When avoid tearing function is enabled, configure the RGB bus according to the LVGL configuration
+    ESP_PanelBus_RGB *rgb_bus = static_cast<ESP_PanelBus_RGB *>(panel->getLcd()->getBus());
+    rgb_bus->configRgbFrameBufferNumber(LVGL_PORT_DISP_BUFFER_NUM);
+    rgb_bus->configRgbBounceBufferSize(LVGL_PORT_RGB_BOUNCE_BUFFER_SIZE);
+#endif
+    panel->begin();
+
+    Serial.println("Initialize LVGL");
+    lvgl_port_init(panel->getLcd(), panel->getTouch());
+
+    Serial.println("Create UI");
+    /* Lock the mutex due to the LVGL APIs are not thread-safe */
+    lvgl_port_lock(-1);
+
+    /* Create a simple label */
+    lv_obj_t *label = lv_label_create(lv_scr_act());
+    lv_label_set_text(label, title.c_str());
+    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+
+    ui_init();
+
+    lvgl_port_unlock();
+
+    Serial.println(title + " end");
+}
+
+void loop()
+{
+    Serial.println("IDLE loop");
+    delay(1000);
+}
